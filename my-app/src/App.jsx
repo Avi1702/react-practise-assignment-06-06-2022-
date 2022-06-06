e8c120f236624231fa69740297b250033b5663c5
Avi1702 <avinashroy1702@gmail.com>
2022-06-07 00:31:58 +0530

    completed

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 import React from "react"
+import { toBeEmpty } from '@testing-library/jest-dom/dist/matchers';
 // import profile from "./components/profile.jsx"
 
 function App() {
@@ -13,17 +14,83 @@ function App() {
   function decrease(){
     updatedAge(age-1)
   }
+
+  function moreDetails(){
+
+    if(document.getElementById("more_details").innerHTML!=""){
+    
+
+    document.getElementById("more_button").innerText="Show More Details"
+    document.getElementById("more_details").innerHTML=""
+  }
+  else
+  {
+    document.getElementById("more_button").innerText="Hide More Details"
+    let p1=document.createElement("p")
+    p1.innerText="Location : Hyderabad"
+
+    let p2=document.createElement("p")
+    p2.innerText="Status : Studing"
+
+    document.getElementById("more_details").append(p1,p2)
+    
+  }
+  }
+
+  function add_note(){
+  let input_value=document.getElementById("add_note").value
+  let box=document.createElement("div")
+  box.setAttribute("id","display_list")
+  box.style.color="green"
+  let checkbox=document.createElement("input")
+ checkbox.setAttribute("type","checkbox")
+
+ box.append(checkbox,input_value)
+document.getElementById("list").append(box)
+
+  checkbox.addEventListener("click",function(){
+   if(box.style.color==="green"){ 
+   box.style.color="red"
+   box.style.textDecoration="line-through"
+   }
+   else{
+     box.style.color="green"
+     box.style.textDecoration="none"
+   }
+    
+  })
+
+  
+  }
   return (
-  <div  style={{width:"40%", padding:"30px 0px 0px 60px", boxShadow:"rgba(0, 0, 0, 0.35) 0px 5px 15px", marginTop:"50px"}}>
+
+  <div id="main">
+
+    {/* the left part */}
+
+  <div id="left" style={{boxShadow:"rgba(0, 0, 0, 0.35) 0px 5px 15px", marginTop:"50px"}}>
     <h1>Profile</h1>
-    <img style={{width:"100px",height:"100px"}} src="https://www.businessinsider.in/photo/79599630/elon-musk-makes-clear-his-stance-on-self-driving-cars-ai-oversight-and-his-ad-for-mars.jpg?imgsize=2436013"></img>
+    <img style={{width:"120px",height:"120px", marginLeft:"32%"}} src="https://www.businessinsider.in/photo/79599630/elon-musk-makes-clear-his-stance-on-self-driving-cars-ai-oversight-and-his-ad-for-mars.jpg?imgsize=2436013" alt=""></img>
     <div>Name : Avinash Kumar</div>
     <div>Age:{age}</div>
     <button onClick={increase}>Increase Age</button>
     <button onClick={decrease}>Decrease Age</button>
-    <br></br>
-    <button>More Details</button>
-    {/* console.log("hi") */}
+
+    <button id="more_button" onClick={moreDetails}>Show More Details</button>
+    <ul id="more_details">
+
+    </ul>
+ 
+  </div>
+
+{/* The right part */}
+<div id='right'>
+  <h1>To Do List</h1>
+  <input type={"text"} placeholder="Add a Note" style={{width:"60%"}} id="add_note" ></input>
+  <button onClick={add_note}>Add</button>
+  <div id='list'></div>
+</div>
+
   </div>
   );
 }
